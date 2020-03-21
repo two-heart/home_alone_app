ec9c27131a93b56698b8b3e79f7bfbb02d8ec660
Andreas Schattney <a.schattney@gmail.com>
2020-03-21 14:39:33 +0100

    Adjust styling of challenge tile

--- home_alone/lib/view/widgets/challenge/challenge_tile.dart
+++ home_alone/lib/view/widgets/challenge/challenge_tile.dart
@@ -10,17 +10,31 @@ class ChallengeTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       child: Container(
-        color: Colors.white,
-        child: ListTile(
-          onTap: () => onTap != null && onTap(challenge),
-          title: Text(
-            challenge.name,
-            style: TextStyle(color: Colors.black87, fontSize: 24),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(2.0)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 4,
+                  spreadRadius: 2,
+                  offset: Offset(2, 2))
+            ]),
+        child: Padding(
+          child: ListTile(
+            onTap: () {
+              if (onTap != null) onTap(challenge);
+            },
+            title: Text(
+              challenge.name,
+              style: TextStyle(color: Colors.black87, fontSize: 24),
+            ),
+            subtitle: Text(
+              challenge.description,
+              style: TextStyle(color: Colors.black87, fontSize: 14),
+            ),
           ),
-          subtitle: Text(
-            challenge.description,
-            style: TextStyle(color: Colors.black87, fontSize: 14),
-          ),
+          padding: EdgeInsets.all(8),
         ),
       ),
       padding: EdgeInsets.all(8),
