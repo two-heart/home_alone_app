edd0751e94aa41a556879dd8d15a1e101a959069
Andreas Schattney <a.schattney@gmail.com>
2020-03-21 19:36:59 +0100

    Fix inkwell not working on ListTile.

--- home_alone/lib/view/widgets/challenge/challenge_tile.dart
+++ home_alone/lib/view/widgets/challenge/challenge_tile.dart
@@ -10,31 +10,25 @@ class ChallengeTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       child: Container(
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(2.0)),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 4,
-                  spreadRadius: 2,
-                  offset: Offset(2, 2))
-            ]),
         child: Padding(
-          child: ListTile(
-            onTap: () {
-              if (onTap != null) onTap(challenge);
-            },
-            title: Text(
-              challenge.name,
-              style: TextStyle(color: Colors.black87, fontSize: 24),
-            ),
-            subtitle: Text(
-              challenge.description,
-              style: TextStyle(color: Colors.black87, fontSize: 14),
+          padding: EdgeInsets.only(left: 8, right: 8),
+          child: Material(
+            elevation: 4.0,
+            child: ListTile(
+              contentPadding: EdgeInsets.all(8),
+              onTap: () {
+                if (onTap != null) onTap(challenge);
+              },
+              title: Text(
+                challenge.name,
+                style: TextStyle(color: Colors.black87, fontSize: 24),
+              ),
+              subtitle: Text(
+                challenge.description,
+                style: TextStyle(color: Colors.black87, fontSize: 14),
+              ),
             ),
           ),
-          padding: EdgeInsets.all(8),
         ),
       ),
       padding: EdgeInsets.all(8),
