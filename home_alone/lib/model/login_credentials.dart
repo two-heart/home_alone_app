cd5bf6b8d59fff8dd20c7f7c7cb1d3a74549aba1
sevenmaster <seven-master@hotmail.de>
2020-03-21 11:48:55 +0100

    made user and login credentials serializable

--- home_alone/lib/model/login_credentials.dart
+++ home_alone/lib/model/login_credentials.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'login_credentials.g.dart';
+
+@JsonSerializable(nullable: true)
 class LoginCredentials {
   LoginCredentials(
     this.email,
@@ -5,4 +10,10 @@ class LoginCredentials {
   );
   final String email;
   final String password;
+
+  factory LoginCredentials.fromJson(Map<String, dynamic> json) =>
+      _$LoginCredentialsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LoginCredentialsToJson(this);
 }
+
